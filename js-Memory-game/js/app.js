ea5d67f976a379edecbef73287211d2091c79fd5
Rugile Petrukauskaite <p998uhp@fspa.myntet.se>
2018-04-17 11:41:29 +0300

    Added readme

--- js-Memory-game/js/app.js
+++ js-Memory-game/js/app.js
@@ -10,147 +10,140 @@ let starsCount;
 
 //Mix cards
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 // Mix cards and reset all status/classes
-function startGame(){
-    cardsList = $(".card");
-    let shuffledCards = shuffle(cardsList);
+function startGame() {
+  cardsList = $(".card");
+  let shuffledCards = shuffle(cardsList);
 
-    movesCount = 0;
-    starsCount = 3;
+  movesCount = 0;
+  starsCount = 3;
 
-    $("#moves").text(movesCount);
-    $("#stars").text(starsCount);
-    
+  $("#moves").text(movesCount);
+  $("#stars").text(starsCount);
 
-    shuffledCards.each(function(index) {
-        $(this).appendTo(parent);
-    });
+  shuffledCards.each(function(index) {
+    $(this).appendTo(parent);
+  });
 
-    resetClasses();
-
+  resetClasses();
 }
 
 // Resets all classes to default
-function resetClasses(){
-
-  $(".card").removeClass( "rotated" );
-  $(".card").removeClass( "matched" );
-  $(".card").removeClass( "unmatched" );
-
-
+function resetClasses() {
+  $(".card").removeClass("rotated");
+  $(".card").removeClass("matched");
+  $(".card").removeClass("unmatched");
 
   stars = $(".fa-star-o");
-  $(stars).removeClass("fa-star-o").addClass("fa-star");
-
+  $(stars)
+    .removeClass("fa-star-o")
+    .addClass("fa-star");
 }
 
 // Rotate card to see icon
-function flipCard(card){
-
-    if (card.hasClass("front")){
-        card.parent().addClass( "rotated" );
-        addOpenedCardToList(card.parent());
-    }else{
-        card.removeClass( "rotated" );
-    }
-
+function flipCard(card) {
+  if (card.hasClass("front")) {
+    card.parent().addClass("rotated");
+    addOpenedCardToList(card.parent());
+  } else {
+    card.removeClass("rotated");
+  }
 }
 
 // Checks if cards is matched or not
-function addOpenedCardToList (openedCard) {
-    openedCards.push(openedCard);
+function addOpenedCardToList(openedCard) {
+  openedCards.push(openedCard);
 
-    if (openedCards.length == 2){
-      increaseMoves();
-        if (openedCards[0].attr("type") === openedCards[1].attr("type")){
-            match(openedCards);
-        }else{
-            unmatch();
-        }
+  if (openedCards.length == 2) {
+    increaseMoves();
+    if (openedCards[0].attr("type") === openedCards[1].attr("type")) {
+      match(openedCards);
+    } else {
+      unmatch();
     }
-
- }
-
- // Increase moves count
- function increaseMoves() {
-    movesCount++;
-    $("#moves").text(movesCount);
-   if ((movesCount === 5) || (movesCount === 10)){
-     removeStar();
-   }
-
- }
-
-  // Remove start when too many moves done
- function removeStar(){
-    starsCount--;
-    console.log(movesCount + " ads " + starsCount);
-    $("#stars").text(starsCount);
-   stars = $(".fa-star");
-   $(stars[stars.length-1]).removeClass("fa-star").addClass("fa-star-o");
- }
-
- // If cards are matched, it leaves opened
- function match(openedCards){
-    openedCards.map( card => card.addClass( "matched" ));
-    openedCards.pop(openedCards[0]);
-    openedCards.pop(openedCards[1]);
-    matchedCards.push(openedCards[0]);
-    matchedCards.push(openedCards[1]);
-
-    if (matchedCards.length == 16){
-      win();
-    }
-
- }
-
-  // If cards unmatched, it should be flip again
- function unmatch(){
-   openedCards.map( card => card.addClass( "unmatched" ));
-   cardsList.off("click");
-
-    setTimeout(function(){
-        openedCards.map( card => {
-          card.removeClass( "rotated unmatched" )
-        });
-        openedCards = [];
-        cardsList.on("click", (e) => handleCardClick());
-    }, 1000);
-
+  }
 }
 
-function win(){
-    setTimeout(function(){
-        $("#ex1").modal();
-        $("#ex1 #stars").text(starsCount);
-        $("#ex1 #moves").text(movesCount);
-    }, 500);
+// Increase moves count
+function increaseMoves() {
+  movesCount++;
+  $("#moves").text(movesCount);
+  if (movesCount === 5 || movesCount === 10) {
+    removeStar();
+  }
 }
 
-function handleCardClick(){
+// Remove start when too many moves done
+function removeStar() {
+  starsCount--;
+  console.log(movesCount + " ads " + starsCount);
+  $("#stars").text(starsCount);
+  stars = $(".fa-star");
+  $(stars[stars.length - 1])
+    .removeClass("fa-star")
+    .addClass("fa-star-o");
+}
+
+// If cards are matched, it leaves opened
+function match(openedCards) {
+  openedCards.map(card => card.addClass("matched"));
+  openedCards.pop(openedCards[0]);
+  openedCards.pop(openedCards[1]);
+  matchedCards.push(openedCards[0]);
+  matchedCards.push(openedCards[1]);
+
+  if (matchedCards.length == 16) {
+    win();
+  }
+}
+
+// If cards unmatched, it should be flip again
+function unmatch() {
+  openedCards.map(card => card.addClass("unmatched"));
+  cardsList.off("click");
+
+  setTimeout(function() {
+    openedCards.map(card => {
+      card.removeClass("rotated unmatched");
+    });
+    openedCards = [];
+    cardsList.on("click", e => handleCardClick());
+  }, 1000);
+}
+
+function win() {
+  setTimeout(function() {
+    $("#ex1").modal();
+    $("#ex1 #stars").text(starsCount);
+    $("#ex1 #moves").text(movesCount);
+  }, 500);
+}
+
+function handleCardClick() {
   let target = $(event.target);
 
-  if (!target.parent().hasClass("matched")){
-      flipCard($(event.target));
+  if (!target.parent().hasClass("matched")) {
+    flipCard($(event.target));
   }
-
 }
 
 startGame();
 
-cardsList.on("click", (e) => handleCardClick());
+cardsList.on("click", e => handleCardClick());
 refresh.on("click", () => startGame());
-startAgain.on("click", () => window.location.href = "index.html");
+startAgain.on("click", () => (window.location.href = "index.html"));
---
+++ js-Memory-game/readme.md
@@ -0,0 +1,15 @@
+# Memory Game
+
+Browser-based card matching game (also known as Concentration).
+
+## Getting Started
+
+No prerequest for this page needed, just hit index.html file and start a game!
+
+## Built With
+
+* [JQuery](https://jquery.com/) - The web framework used
+
+## Authors
+
+* **Rugile Petrukauskaite** - *Initial work* - [PurpleBooth](https://github.com/Rugile1276)
